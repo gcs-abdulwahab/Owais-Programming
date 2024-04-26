6bf95edc32ac2d143b2cc04cfc14b9abe4071731
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-04-26 21:12:12 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,9 @@
-print("Welcome to python")
+# day 10
+# chapter 4 page 123
+# book 3
+
+sd = 1000 + 400
+ld = 3500
+perday_school = sd * 2
+print("distance from amna's home to school is", sd, "meters")
+print("In one day amna  travels school ", perday_school, "meters")
