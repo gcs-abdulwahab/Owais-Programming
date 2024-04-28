cf66efb6b06ade1637a7f52cdb6b9567330c1a14
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-04-28 21:31:03 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -2,8 +2,13 @@
 # chapter 4 page 123
 # book 3
 
-sd = 1000 + 400
-ld = 3500
-perday_school = sd * 2
-print("distance from amna's home to school is", sd, "meters")
+school_distance = 1000 + 400
+library_distance = 3500
+perday_school = school_distance * 2
+perday_library = library_distance * 2
+total_distance = perday_library * 2 + perday_school * 5
+print("distance from amna's home to school is", school_distance, "meters")
 print("In one day amna  travels school ", perday_school, "meters")
+print("Distance from Amna's home to library is ", library_distance)
+print("Amna traveled home to library ", perday_library, "meters")
+print("Amna traveled total meters in one week",total_distance)
