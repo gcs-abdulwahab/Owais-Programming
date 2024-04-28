c63ec971d2da10487f79059cd89a721643e34a17
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-04-28 22:01:44 +0500

    shirts sum

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,14 +1,6 @@
-# day 10
-# chapter 4 page 123
-# book 3
+s1 = 20
+s2 = 27
+total = 50
 
-school_distance = 1000 + 400
-library_distance = 3500
-perday_school = school_distance * 2
-perday_library = library_distance * 2
-total_distance = perday_library * 2 + perday_school * 5
-print("distance from amna's home to school is", school_distance, "meters")
-print("In one day amna  travels school ", perday_school, "meters")
-print("Distance from Amna's home to library is ", library_distance)
-print("Amna traveled home to library ", perday_library, "meters")
-print("Amna traveled total meters in one week",total_distance)
+remaning = total - (s1 + s2)
+print("remaning money is ",remaning)
