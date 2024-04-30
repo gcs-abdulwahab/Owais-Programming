78b8dcb74d7cd80b7e2a34e4f973e29e15067ac2
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-04-30 23:18:58 +0500

    table of x

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,6 +1,24 @@
-x = 45
-y = 3
-q = x // y
-rem = x % y
-print(f" {x} // {y} is   {q}")
-print(f" {x} % {y} is  {rem}  ")
+# It's a table of 3  up to 10
+x = 63
+print(f"{x} * 1 ={x * 1}")
+print(f"{x} * 2 = {x * 2}")
+print(f"{x} * 3 = {x * 3}")
+print(f"{x} * 4 = {x * 4}")
+print(f"{x} * 5 = {x * 5}")
+print(f"{x} * 6 = {x * 6}")
+print(f"{x} * 7 = {x * 7}")
+print(f"{x} * 8 = {x * 8}")
+print(f"{x} * 9 = {x * 9}")
+print(f"{x} * 10 = {x * 10}")
+
+
+
+
+
+
+
+
+
+
+
+
