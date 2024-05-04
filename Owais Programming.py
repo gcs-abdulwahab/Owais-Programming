9cce38c3523175ea56d456b8ccd7dba26d01cf72
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-04 07:47:04 +0500

    Owais Arrays  of different ellement

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,4 +1,15 @@
-stationary = ["pencil", "eraser", "marker", "pen"]
-n = len(stationary)
-print(stationary[0])
-print(stationary[n - 1])
+items = ["chocolate", "milo", "lays", "loly pop", "candy", "marsh melo"]
+planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+fruits = ["mango", "apple", "orange", "pineapple", "watermelon", "banana", "dragonfruit"]
+tp = len(planets)
+print(planets)
+
+# print(f"{planets[2]} is our planet")
+print("We have", tp, "planets")
+# print(f"{planets[tp - 1]}")
+middle =  tp // 2
+
+print(planets[middle ])
+
+
+
