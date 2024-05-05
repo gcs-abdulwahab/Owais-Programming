d3942890eae90bd9b63d9c61c51fab4381ff0650
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-05 22:34:48 +0500

    Even Odd Question

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,5 +1,7 @@
-x = 5
-if x == 2:
-    print("Equal")
+x = 55156525212905
+print("quotient is", x // 2)
+print("remainder is", x % 2)
+if x % 2 == 0:
+    print("even")
 else:
-    print("Not equal")
+    print("odd")
