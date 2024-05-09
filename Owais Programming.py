72eaeb0c8ea46976b5f596b24cc165219f517450
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-09 23:29:17 +0500

    table of 5

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,13 @@
-print("Welcome to python")
+# table of 5 upto to 10
+
+x = 5
+print(f"{x} * 1 = {x}")
+print(f"{x} * 2 = {x * 2}")
+print(f"{x} * 3 = {x * 3}")
+print(f"{x} * 4 = {x * 4}")
+print(f"{x} * 5 = {x * 5}")
+print(f"{x} * 6 = {x * 6}")
+print(f"{x} * 7 = {x * 7}")
+print(f"{x} * 8 = {x * 8}")
+print(f"{x} * 9 = {x * 9}")
+print(f"{x} * 10 = {x * 10}")
