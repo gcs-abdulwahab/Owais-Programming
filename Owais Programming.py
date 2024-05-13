e2c86fdd19e99cc04d0841925b6493e2f68eb180
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-13 20:12:45 +0500

    simplify chained expression

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,11 +1,11 @@
-marks = 40
+marks = 65
 if marks > 90:
     print("pass with excellent grade")
-if marks > 80:
+if 80 < marks < 90:
     print("pass with very good grade")
-if marks > 70:
+if 70 < marks < 80:
     print("pass with good grade")
-if marks > 60:
+if 60 < marks < 70:
     print("pass with satisfactory")
 if marks < 60:
     print("fail")
