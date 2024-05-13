fa453fafac2bf894d272556384be1b1d45868d9e
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-13 20:15:33 +0500

    less than equal to case

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,11 +1,11 @@
-marks = 65
+marks = 70
 if marks > 90:
     print("pass with excellent grade")
-if 80 < marks < 90:
+if 80 <= marks < 90:
     print("pass with very good grade")
-if 70 < marks < 80:
+if 70 <= marks < 80:
     print("pass with good grade")
-if 60 < marks < 70:
+if 60 <= marks < 70:
     print("pass with satisfactory")
 if marks < 60:
     print("fail")
