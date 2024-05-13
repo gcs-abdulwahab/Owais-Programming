d5bcf939141a74fc64a7d604e918cb9949295923
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-13 20:02:12 +0500

    grades

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,11 @@
-print("Welcome to python")
+marks = 40
+if marks > 90:
+    print("pass with excellent grade")
+if marks > 80:
+    print("pass with very good grade")
+if marks > 70:
+    print("pass with good grade")
+if marks > 60:
+    print("pass with satisfactory")
+if marks < 60:
+    print("fail")
