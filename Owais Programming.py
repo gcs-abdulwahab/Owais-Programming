d9b9e435fc44746bc3b40d349ced482dab4eb7ce
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-17 20:14:02 +0500

    input method
    input method

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,12 @@
-print("Welcome to python")
+grade = input("Give me my grade?")
+# grade = 'D'
+if grade == 'A':
+    print("excellent")
+if grade == 'B':
+    print("very good")
+if grade == 'C':
+    print("good")
+if grade == 'D':
+    print("satisfying")
+if grade == 'F':
+    print("fail")
