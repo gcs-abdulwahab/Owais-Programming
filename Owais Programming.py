13f6551608d8d824b06cd8406bf26523848fbc2a
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-05-21 17:54:57 +0500

    even odd

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,5 +1,5 @@
-# n1 = int(input("give me the first nuber"))
-# n2 = int(input("give me the second number"))
-n1 = 2
-n2 = 3
-print(f"{n1} + {n2} = {n1 + n2}")
+n1 = int(input("give me an number"))
+if n1 % 2 == 0:
+   print("even")
+else:
+   print("odd")
