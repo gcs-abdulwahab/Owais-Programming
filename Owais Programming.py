4e8f6a0737086e48e90554def335a01812ddbf12
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-04 19:40:07 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,9 +1,5 @@
 x = 10
 y = 15
 z = 20
-if x < y and x < z:
-    print(f"{x} is the smallest")
-if y < x and y < z:
-    print(f"{y} is the smallest")
-if z < y and z < x:
-    print(f"{z} is the smallest")
+
+print("minimum is ", min(x, y, z))
