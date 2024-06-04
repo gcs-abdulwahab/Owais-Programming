8cf302d55db2e85c786950ef641c48d00d65d9fb
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-04 16:09:15 +0200

    smallest amongst three

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,8 +1,9 @@
 x = 10
+y = 15
 z = 20
-if x > z:
-    print(f"{x} is greater than {z}")
-if z > x:
-    print(f"{z} is greater than {x}")
-if x == z:
-    print(f"{z} and {x} are the same")
+if x < y and x < z:
+    print(f"{x} is the smallest")
+if y < x and y < z:
+    print(f"{y} is the smallest")
+if z < y and z < x:
+    print(f"{z} is the smallest")
