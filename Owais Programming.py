3c292f5853db53895aad5771944cbb38c1768cb9
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-19 15:22:17 +0500

    fruits array

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,4 @@
-print("Welcome to python")
+fruits = ["mango", "watermelon","appricot", "banana", "apple", "coconut", "pear", "dragon fruit"]
+for fruit in fruits:
+    if fruit[0] == 'a':
+        print(fruit)
