d1b704de190872b1e453aa6f10c31e236dccec3c
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-22 20:17:45 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,4 @@
-print("Welcome to python")
+friends = ["shafae", "majeed", "rasheed", "amaan", "ali", "rafae", "subhan"]
+for friend in friends:
+    if friend[len(friend) - 1] == 'e':
+        print(friend)
