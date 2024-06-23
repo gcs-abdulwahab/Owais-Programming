37d43ee75bea7e48187eac9d8132d7f3a773ff10
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-23 12:00:08 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,4 +1,4 @@
 friends = ["amna", "shafae", "noman", "majeed", "rasheed", "amaan", "ali", "rafae", "subhan"]
 for friend in friends:
-    if len(friend) > 5:
+    if len(friend)in [4, 6, 8]:
         print(friend)
