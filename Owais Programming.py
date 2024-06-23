ae90c7642272fde035dd958a2cc602010bd97068
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-23 17:09:52 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,4 +1,4 @@
-numbers = [565, 1, 467, 5, 8, 447, 656, 767, 676, 7, 10, 50, 1, 45]
+numbers = [565, 1, 467, 5, 8, 447, 656, 12, 24, 767, 676, 7, 10, 50, 1, 45]
 for number in numbers:
-    if number % 5 == 0:
+    if number % 3 == 0 and number % 4 == 0:
         print(f"{number} ")
