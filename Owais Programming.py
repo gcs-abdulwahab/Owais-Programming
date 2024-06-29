61d10418e6e02b8fa3eda119aea4bbc7b23fdeb4
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-29 21:29:44 +0500

    type casting into integer

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,5 +1,6 @@
-n = 110
-if n % 2 == 0:
-    print("even")
+n = int(input("give me an number "))
+
+if n % 5 == 0:
+    print("number is if for divisible by 5")
 else:
-    print("odd")
+    print("number is not divisible by 5")
