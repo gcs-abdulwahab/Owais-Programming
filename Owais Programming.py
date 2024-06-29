113d98ca7f3e39904055cdd977998804296be0f3
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-29 21:10:39 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,7 +1,5 @@
-marks = 39
-if marks <= 39:
-    print("fail")
-if 40 <= marks <= 49:
-    print("conditionally passed")
-if marks >= 50:
-    print("pass")
+n = 110
+if n % 2 == 0:
+    print("even")
+else:
+    print("odd")
