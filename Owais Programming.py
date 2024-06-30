1e19d30910147cb81bed1be8a9699a70ff7f3c9e
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-30 11:21:05 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,6 +1,3 @@
-n = int(input("give me an number "))
-
-if n % 5 == 0:
-    print("number is if for divisible by 5")
-else:
-    print("number is not divisible by 5")
+n = "Muhammad Owais"
+x = len(n)
+print(x)
