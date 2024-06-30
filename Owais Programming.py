0907f21b85830e0678a77a7169e41f812b71058b
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-06-30 11:40:23 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,9 @@
-print("Welcome to python")
+x = 5
+y = 10
+z = 12
+if x < z and x < y:
+    print("x is the smallest")
+if y < x and y < z:
+    print("y is the smallest")
+if z < y and z < x:
+    print("z is the smallest")
