ae5f3301b138149c51762dd7c541d075ed467e42
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-07-01 18:57:02 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,9 +1,4 @@
-x = 5
-y = 10
-z = 12
-if x < z and x < y:
-    print("x is the smallest")
-if y < x and y < z:
-    print("y is the smallest")
-if z < y and z < x:
-    print("z is the smallest")
+list = [43, 44, 23, 66, 77, 32, 242, 12, 6]
+for l in list:
+    if l % 2 == 0:
+        print(l)
