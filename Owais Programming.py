57838654d26e1eb5bd614cb448499f0362eb9d31
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-07-03 07:33:26 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,7 +1,25 @@
+def area(c, d):
+    return c * d
 
 
-def square(x):
-    return x * x
+print(area(5, 6))
+
+print("and")
 
 
-print(square(4))
+def get_average(a, b):
+    res = (a + b) // 2
+    return res
+
+
+print(get_average(5, 15))
+
+print("and")
+
+
+def perimeter_of_a_rectangle(x, y):
+    ret = (x * 2) + (y * 2)
+    return ret
+
+
+print(perimeter_of_a_rectangle(2,3))
