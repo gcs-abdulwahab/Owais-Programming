1c30467e5bf03c4459b57642b5f9c1f6e821e71b
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-07-19 21:58:04 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,2 +1,12 @@
-for i in range(1,6+1):
-    print("*"* i)
+def is_even(x):
+    if x % 2 == 0:
+        return True
+
+    else:
+        return False
+
+x = 33
+if is_even(x):
+    print("I am even.")
+else:
+    print("I am not even")
