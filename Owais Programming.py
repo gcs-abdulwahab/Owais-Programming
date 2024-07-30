0ac0ce59b8a6a80c8a9133de696381ff091684ac
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-07-30 18:12:22 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,6 +1,10 @@
-def print_square(w):
+def print_square(w,c):
     for i in range(w):
-        print(f"* " * w)
+        print(f"{c} "  * w)
 
-print_square(5)
+# print_square(4,'*')
 
+numbers_list = [1,2,3,4,5,6,7,8,9,10,]
+for item in numbers_list:
+    print_square(item,'* ')
+    print(".......................")
