92b0e05d656c8cdbd973bb04233bc852fcc32967
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-08-01 19:45:55 +0500

    leap year

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,13 @@
-print("Hello World")
+list = [1972, 2000, 2025, 1964, 2004, 1992, 1994, 1996]
+def is_leap_year(x):
+    if x % 4 == 0:
+        return True
+    else:
+        return False
+
+
+for x in list:
+    if is_leap_year(x)== True:
+        print(f" {x}  is a Leap Year")
+    else:
+        print(f" {x}  is not a Leap Year")
