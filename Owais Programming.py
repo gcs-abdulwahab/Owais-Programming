c3dcc9520dbaf478f3c2180420b7bc352fae203a
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-08-01 20:08:49 +0500

    cube from list

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,9 +1,9 @@
 list = [2 , 4 , 6 , 8 , 10]
 
-x= 3
+
 def cube(x):
     return x * x * x
 
 
-for i in range(1,11):
-    print(f"This the cube of {i} is {cube(i)}")
+for item in list:
+    print(f"The the cube of {item} is {cube(item)}")
