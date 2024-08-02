51e79e400de3b402a515ed0088f26c0ed65f0708
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-08-02 22:03:26 +0500

    Update Owais Programming.py

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,22 @@
-print("Hello World")
+def square(x):
+    return x * x
+
+
+def cube(x):
+    return x * x * x
+
+
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+
+list = [2, 3, 4, 5, 6]
+
+for item in list:
+    if is_even(item):
+        print(f"This item is even so I will print square {square(item)}")
+    else:
+        print(f"This item odd so I will print the cube {cube(item)}")
