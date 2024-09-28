72c0e84694d80068b15d516f6004f66d1e6d86f3
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-09-28 13:06:37 +0500

    added functions and multiples
    added functions of factors and multiples
    
    next is to   filter it further by compound condition

--- Owais Programming.py
+++ Owais Programming.py
@@ -1,1 +1,15 @@
-print("Hello World")
+def factors(n):
+    print(f"factors of {n} :")
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i, end=" , ")
+
+
+def multiple(n):
+    print(f"multiples of {n}:")
+    for i in range(1, 11):
+        print(f" {i * n}", end=' , ')
+
+
+factors(600)
+multiple(6)
