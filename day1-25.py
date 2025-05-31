cab738453c68fd9727c42053eb6657e78a166007
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2025-05-31 09:30:16 +0500

    len function

--- day1-25.py
+++ day1-25.py
@@ -1,6 +1,14 @@
-# --------------------------- division ---------------------------------------
-x = 7
-y = 2
-print(f"     {x} // {y} = ", x // y)
-print(" remainder =  ", x % y)
-# --------------------------
+# --------------------------friends----------------------------------------------------
+name_1 = "Amman"
+name_2 = "Alyan"
+name_3 = "Subhan"
+
+print("best friends list:""\n")
+print("no.1",    name_1,    "name length = ", len(name_1), "\n")
+print("no.2",    name_3,    "name length = ", len(name_3), "\n")
+print("no.3",    name_2,    "name length = ", len(name_2), "\n")
+
+
+
+
+
